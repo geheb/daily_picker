b649d1a429a88c02720d61de447fb4bfe88e70cd
Thomas <thomas.geheb@gmail.com>
2022-04-02 00:35:13 +0200

    * extract team members as a separate file * persist not available members * select next member if not available

---
+++ team.js
@@ -0,0 +1,11 @@
+var team = [
+  {"id": 1, "name": "Thomas",    "img": "https://loremflickr.com/320/240?random=1" },
+  {"id": 2, "name": "Katrin",    "img": "https://loremflickr.com/320/240?random=2" },
+  {"id": 3, "name": "Christian", "img": "https://loremflickr.com/320/240?random=3" },
+  {"id": 4, "name": "Kerstin",   "img": "https://loremflickr.com/320/240?random=4" },
+  {"id": 5, "name": "Julia",     "img": "https://loremflickr.com/320/240?random=5" },
+  {"id": 6, "name": "Daniel",    "img": "https://loremflickr.com/320/240?random=6" },
+  {"id": 7, "name": "Celine",    "img": "https://loremflickr.com/320/240?random=7" },
+  {"id": 8, "name": "Luca",      "img": "https://loremflickr.com/320/240?random=8" },
+  {"id": 9, "name": "Annabell",  "img": "https://loremflickr.com/320/240?random=9" }
+];
